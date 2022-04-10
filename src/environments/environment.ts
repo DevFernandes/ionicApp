0a9e5cc6d6a9326e54734872e858bef4b9e5d984
Matheus Fernandes <103370734+watchwatchyeah@users.noreply.github.com>
2022-04-10 11:25:07 -0300

    Update

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyA-AXY6zgvEN_UunKwbmbBc6Ho9qdy314c",
+    authDomain: "projeto-integrador-12b61.firebaseapp.com",
+    databaseURL: "https://projeto-integrador-12b61.firebaseio.com",
+    projectId: "projeto-integrador-12b61",
+    storageBucket: "projeto-integrador-12b61.appspot.com",
+    messagingSenderId: "402942265849",
+    appId: "1:402942265849:web:eaf356d7f04936b2dad528"
+  }
+};
